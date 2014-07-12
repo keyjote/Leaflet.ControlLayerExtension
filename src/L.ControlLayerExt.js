b43b0829a0ee04feb8e27740689e71731c24594d
keyjote <dgarciach@gmail.com>
2014-07-12 13:03:13 +0200

    here is the extension for the l.control.layer

---
+++ src/L.ControlLayerExt.js
@@ -0,0 +1,43 @@
+/*
+ * L.ControlLayerExt is an extention for the L.control.layer, this will allow the controller to select different layers programatically.
+ */
+
+L.ControlLayerExt = L.Control.Layers.extend({
+    
+    pickLayer: function (layerName) {
+        var layerId = 0;
+
+        for(var i in this._layers) {
+            if (this._layers[i].name == layerName) {
+                layerId = i;
+                break;
+            }
+        }
+
+        if (layerId != 0) {
+            var elements = this._form.elements;
+            for(var index in elements) {
+                if (elements[index].layerId == layerId) {
+                    elements[index].click();
+                    return;
+                }
+            }
+        }
+    },
+
+    pickLayerByPosition: function (layerPos) {
+        if (isNaN(layerPos) || layerPos < 0) {
+            return;
+        }
+
+        var elements = this._form.elements;
+        if (layerPos < elements.length) {
+            elements[layerPos].click();
+            return;
+        }
+    },
+});
+
+L.controlLayerExt = function (baseLayers, overlays, options) {
+	return new L.ControlLayerExt(baseLayers, overlays, options);
+};
